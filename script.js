ff47f06ac2be4b3b3b26be06518e83619401c72e
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-29 23:20:37 +0100

    [UPTADE] Problem solved

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let user_mail=prompt('Inserisca la sua e-mail:');
 let flag=false;
 
 //controllare se la mail inserita dall'utente si trova all'interno dell'array 
-for(let i=0; i>email_autorizzate.length; i++){
+for(let i=0; i<email_autorizzate.length; i++){
     // se laq trovo la variabile di controllo duventa vera
     if(email_autorizzate[i] == user_mail){
         flag=true;
