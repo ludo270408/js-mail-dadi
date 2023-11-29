8b8f3af7bbc5b2ff8e531a2f16cf779ebbeda15d
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-29 23:04:42 +0100

    [UPTADE]  Searching the mail in the array

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ let user_mail=prompt('Inserisca la sua e-mail:');
 
 //variabile di controllo
 let flag=false;
+
+//controllare se la mail inserita dall'utente si trova all'interno dell'array 
+for(let i=0; i>email_autorizzate.length; i++){
+    // se laq trovo la variabile di controllo duventa vera
+    if(email_autorizzate[i] == user_mail){
+        flag=true;
+    }
+}
