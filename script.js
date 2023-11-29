92585cf5dee08561177d323c1fd20443a8e57c01
ludo270408 <ludovica.sette7@gmaio.com>
2023-11-29 23:00:29 +0100

    [UPTADE] Mail asked and control variable added

--- script.js
+++ script.js
@@ -4,3 +4,7 @@
 const email_autorizzate=['luca@gmail.com', 'mario@gmail.com', 'giulia@gmail.com'];
 
 //Chuedere la mail all'utente
+let user_mail=prompt('Inserisca la sua e-mail:');
+
+//variabile di controllo
+let flag=false;
